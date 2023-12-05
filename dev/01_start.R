736f15251f60df255ebab47dadb9e3c6b6ba4cc3
Allan Victor <62240802+Allan-gitrepos@users.noreply.github.com>
2023-12-05 22:05:08 +0530

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -37,12 +37,12 @@ golem::install_dev_deps()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("ALLAN") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 devtools::build_readme()
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "ALLAN")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
